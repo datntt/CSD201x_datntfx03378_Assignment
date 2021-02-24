c50a38b2729a42a660e18cf8ab005caa7dc8bc49
datntt <you@example.com>
2021-02-24 08:21:35 +0700

    dat up ass3_tree

--- Assignment/ASM3/src/Main.java
+++ Assignment/ASM3/src/Main.java
@@ -12,9 +12,9 @@ public class Main {
             System.out.println("1. Insert a new Person");
             System.out.println("2. Inorder traverse");
             System.out.println("3. Breadth-First Traversal traverse");
-            System.out.println("4.Search by Person ID");
+            System.out.println("4. Search by Person ID");
             System.out.println("5. Delete by Person ID");
-            System.out.println("6.  Balancing Binary Search Tree");
+            System.out.println("6. Balancing Binary Search Tree");
             System.out.println("7. DFS_Graph");
             System.out.println("8. Dijkstra");
             System.out.println("0. Exit");
